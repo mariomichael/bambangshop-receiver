e7bad7037914d0cfe33a2b5492c582828c4b85fe
Mario <mario.michaeljs21@gmail.com>
2024-04-17 18:13:54 +0700

    Implement subscribe function in Notification service.

--- src/model/subscriber.rs
+++ src/model/subscriber.rs
@@ -2,7 +2,7 @@ use rocket::serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
-pub struct Subscriber {
+pub struct SubscriberRequest {
     pub url: String,
     pub name: String,
 }
